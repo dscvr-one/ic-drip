2041378dd1ebb93db983acb835fc48f9eed39557
Rick Porter <rick.e.porter@gmail.com>
2021-09-08 19:36:35 -0700

    removing gtag

--- src/ic_loot_assets/main.js
+++ src/ic_loot_assets/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "virtual:windi.css";
-import VueGtag from "vue-gtag-next";
 
 /**
  * @dfinity/agent requires this. Can be removed once it's fixed
@@ -10,10 +9,5 @@ window.global = window;
 
 const app = createApp(App);
 
-app.use(VueGtag, {
-  property: {
-    id: "G-KE88EVJHWW",
-  },
-});
 
 app.mount("#app");
